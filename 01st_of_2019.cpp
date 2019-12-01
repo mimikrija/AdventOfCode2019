1b18b1c9d990bf0046d9061e3c984cbda7a72059
Maja Gacesa <maja.gacesa@gmail.com>
2019-12-01 10:47:49 +0100

    [day01] both parts

--- 01st_of_2019.cpp
+++ 01st_of_2019.cpp
@@ -9,6 +9,13 @@
 
 using namespace std;
 
+int FuelCalculation(int Input)
+{
+	int calculation = int(Input / 3) - 2;
+	if (calculation >= 0) return calculation;
+	else return 0;
+}
+
 void Day_01(ifstream& InputFile)
 {
 	int ElfInput;
@@ -19,4 +26,27 @@ void Day_01(ifstream& InputFile)
 	{
 		FuelInstructionList.push_back(ElfInput);
 	}
+
+	int TotalFuelNeeded = 0;
+	int maja = int(14 / 3);
+
+	for_each(FuelInstructionList.begin(), FuelInstructionList.end(),
+		[&TotalFuelNeeded](int ElfInput)
+		{TotalFuelNeeded += FuelCalculation(ElfInput); });
+
+	cout << "Total amount of fuel needed is: " << TotalFuelNeeded << "\n";
+
+	int TotalFuelNeededForReal = 0;
+	for_each(FuelInstructionList.begin(), FuelInstructionList.end(),
+		[&TotalFuelNeededForReal](int ElfInput)
+		{
+			int tempinput = FuelCalculation(ElfInput);
+			while (tempinput > 0)
+			{
+				TotalFuelNeededForReal += tempinput;
+				tempinput = FuelCalculation(tempinput);
+			}
+		});
+
+	cout << "LOL JK, total amount neede is actually: " << TotalFuelNeededForReal << "\n";
 }
