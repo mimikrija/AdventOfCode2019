9abca46dcaa5672e9e33138e65e66909edf027e6
Maja Gacesa <maja.gacesa@gmail.com>
2019-12-02 08:07:59 +0100

    [wip day02]

--- 02nd_of_2019.cpp
+++ 02nd_of_2019.cpp
@@ -5,6 +5,7 @@
 #include <vector>
 #include <iterator>
 #include <algorithm>
+#include <numeric>
 #include "DayHeaders.h"
 
 
@@ -13,7 +14,7 @@ using namespace std;
 
 void Day_02(ifstream& InputFile)
 {
-	vector<int> CodeList;
+	vector<int> CodeList, CleanCodeList;
 
 	// read integers one by one
 	// fill solutions into a vector
@@ -29,19 +30,59 @@ void Day_02(ifstream& InputFile)
 		}
 	}
 
+	CleanCodeList = CodeList;
 	CodeList.at(1) = 12;
 	CodeList.at(2) = 2;
-	
+
+
 	for (int pos = 0; pos < CodeList.size(); pos++)
 	{
 		int result;
-		int code = CodeList.at(pos*4);
+		int code = CodeList.at(pos * 4);
 		if (code == 99) break;
-		int inputpos = CodeList.at(pos*4 + 3);
-		int firstarg = CodeList.at(pos*4 + 1);
+		int inputpos = CodeList.at(pos * 4 + 3);
+		int firstarg = CodeList.at(pos * 4 + 1);
 		int secondarg = CodeList.at(pos * 4 + 2);
 		switch (code)
 		{
+		case 1:
+			CodeList.at(inputpos) = CodeList.at(firstarg) + CodeList.at(secondarg);
+			break;
+		case 2:
+			CodeList.at(inputpos) = CodeList.at(firstarg) * CodeList.at(secondarg);
+			break;
+		default:
+			break;
+		}
+
+	}
+
+	cout << CodeList[0] << "\n";
+
+	// part 2
+
+	int noun = 2;
+	int verb = 13;
+	bool change = true;
+	vector<int> Nouns(CleanCodeList.size() - 1), Verbs(CleanCodeList.size() - 1);;
+	iota(Nouns.begin(), Nouns.end(), 0);
+	iota(Verbs.begin(), Verbs.end(), 0);
+
+	while (CodeList.at(0) != 19690720)
+	{
+		CodeList = CleanCodeList;
+		CodeList.at(1) = Nouns.back();
+		CodeList.at(2) = Verbs.back();
+		for (int pos = 0; pos < CodeList.size(); pos++)
+		{
+			int result;
+			int code = CodeList.at(pos * 4);
+			if (code == 99) break;
+			int inputpos = CodeList.at(pos * 4 + 3);
+			int firstarg = CodeList.at(pos * 4 + 1);
+			int secondarg = CodeList.at(pos * 4 + 2);
+			switch (code)
+			{
 			case 1:
 				CodeList.at(inputpos) = CodeList.at(firstarg) + CodeList.at(secondarg);
 				break;
@@ -50,10 +91,23 @@ void Day_02(ifstream& InputFile)
 				break;
 			default:
 				break;
+			}
+		}
+		if (change)
+		{
+			Nouns.resize(Nouns.size() - 1);
+			change = false;
+		}
+		else
+		{
+			Verbs.resize(Verbs.size() - 1);
+			change = true;
 		}
 		
 	}
+		
 	
-	cout << CodeList[0];
-	// too low 530607
+
+	//while (CodeList[0] != 19690720)
+	cout << 100 * noun + verb;
 }
