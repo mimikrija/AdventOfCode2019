e4e9460dcab06a6bf97eba1bc4cc11172f8b534e
Maja Gacesa <maja.gacesa@gmail.com>
2019-12-02 07:31:43 +0100

    [day02] part 1

--- 02nd_of_2019.cpp
+++ 02nd_of_2019.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <string>
 #include <vector>
 #include <iterator>
@@ -12,8 +13,47 @@ using namespace std;
 
 void Day_02(ifstream& InputFile)
 {
+	vector<int> CodeList;
 
-    // read input line per line
-	vector<string> ListOfInputs{ istream_iterator<string>{InputFile},{} };
+	// read integers one by one
+	// fill solutions into a vector
+	string line;
+	while (getline(InputFile, line))
+	{
+		stringstream linestream(line);
+		string value;
 
+		while (getline(linestream, value, ','))
+		{
+			CodeList.push_back(stoi(value));
+		}
+	}
+
+	CodeList.at(1) = 12;
+	CodeList.at(2) = 2;
+	
+	for (int pos = 0; pos < CodeList.size(); pos++)
+	{
+		int result;
+		int code = CodeList.at(pos*4);
+		if (code == 99) break;
+		int inputpos = CodeList.at(pos*4 + 3);
+		int firstarg = CodeList.at(pos*4 + 1);
+		int secondarg = CodeList.at(pos * 4 + 2);
+		switch (code)
+		{
+			case 1:
+				CodeList.at(inputpos) = CodeList.at(firstarg) + CodeList.at(secondarg);
+				break;
+			case 2:
+				CodeList.at(inputpos) = CodeList.at(firstarg) * CodeList.at(secondarg);
+				break;
+			default:
+				break;
+		}
+		
+	}
+	
+	cout << CodeList[0];
+	// too low 530607
 }
