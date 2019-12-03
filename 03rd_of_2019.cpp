e2e4c0046f6f6f978bf6b96ba171b31c7be6379a
Maja Gacesa <maja.gacesa@gmail.com>
2019-12-03 07:01:02 +0100

    [day03] copy day03 from 2015 for the input

--- 03rd_of_2019.cpp
+++ 03rd_of_2019.cpp
@@ -1,37 +1,99 @@
 #include <iostream>
 #include <fstream>
-#include <sstream>
+#include <string>
 #include <vector>
+#include <set>
+#include <iterator>
 #include <algorithm>
-#include <numeric>
 #include "DayHeaders.h"
 
 using namespace std;
 
+vector<pair<int, int>> VisitedHousesVector(vector<char>Instructions)
+{
+	// convert instructions to coordinates of visited houses
+	// v is south
+	// ^ is north
+	// > is east
+	// < is west
+	vector<pair<int, int>> temp(1, make_pair(0, 0)); // initialize;
+	pair<int, int> CurrentCoordinate;
+	for (auto OneDirection : Instructions)
+	{
+		switch (OneDirection)
+		{
+		case ('<'):
+			CurrentCoordinate.first--;
+			break;
+		case ('>'):
+			CurrentCoordinate.first++;
+			break;
+		case ('v'):
+			CurrentCoordinate.second--;
+			break;
+		case ('^'):
+			CurrentCoordinate.second++;
+			break;
+		}
+		temp.push_back(CurrentCoordinate);
+
+	}
+	return temp;
+}
+
 void Day_03(ifstream& InputFile)
 {
-	int ElfInput;
-	vector<int> FuelEstimateList, FuelEstimateListForReal;
 
-	// read integers one by one
-	// fill solutions into a vector
-	while (InputFile >> ElfInput)
+	char OneDirection;
+	vector<char> WackyElfInstructions;
+
+	while (InputFile >> OneDirection)
 	{
-		FuelEstimateList.push_back(ElfInput);
+		WackyElfInstructions.push_back(OneDirection);
 	}
 
-	vector<int> CodeList;
 
-	// read comma separated data
-	string line;
-	while (getline(InputFile, line))
+	vector<pair<int, int>> VisitedHouses = VisitedHousesVector(WackyElfInstructions);
+	set<pair<int, int>> UniqueCoordinates(VisitedHouses.begin(),VisitedHouses.end());
+	cout << "This Christmas there were " << UniqueCoordinates.size() << " houses which got at least one gift! \n";
+
+
+	// This is me missunderstanding the first part of the puzzle
+	// ****
+	//vector<pair<int, int>> LuckyHouses(UniqueCoordinates.size());
+	//auto it = copy_if(UniqueCoordinates.begin(), UniqueCoordinates.end(), LuckyHouses.begin(), [VisitedHouses](auto House) {return count(VisitedHouses.begin(), VisitedHouses.end(), House) > 1; });
+	//LuckyHouses.resize(distance(LuckyHouses.begin(), it));
+	//cout << "This Christmas there were " << LuckyHouses.size() << " houses which got at least two gifts! \n";
+	// ****
+
+	// second part: Santa and Robo Santa deliver the gifts together
+	vector <char> SantaInstructions, RoboInstructions;
+
+	auto it = WackyElfInstructions.begin();
+	while (it != WackyElfInstructions.end())
 	{
-		stringstream linestream(line);
-		string value;
-
-		while (getline(linestream, value, ','))
+		int check = it - WackyElfInstructions.begin();
+		
+		if (check % 2 == 1)
 		{
-			CodeList.push_back(stoi(value));
+			SantaInstructions.push_back(*it); // odd directions go to Santa;
 		}
+		else
+		{
+			RoboInstructions.push_back(*it); // even directions go to Robo;
+		}
+		it++;
 	}
+	vector <pair<int, int>> SantaHouses, RoboHouses;
+	SantaHouses = VisitedHousesVector(SantaInstructions);
+	RoboHouses = VisitedHousesVector(RoboInstructions);
+
+	auto JointHouses = SantaHouses;
+	JointHouses.insert(JointHouses.end(), RoboHouses.begin(), RoboHouses.end());
+
+	set<pair<int, int>> UniqueJointCoordinates(JointHouses.begin(),JointHouses.end());
+
+	cout << "When Santa and Robosanta work together, " << UniqueJointCoordinates.size() << " houses get at least one gift!\n";
+
+	
 }
