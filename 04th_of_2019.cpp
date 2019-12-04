95c9c372a634a6799b147cafdb8b3d7429a29eca
Maja Gacesa <maja.gacesa@gmail.com>
2019-12-04 07:56:31 +0100

    [day04] part 1 solution - clean me up

--- 04th_of_2019.cpp
+++ 04th_of_2019.cpp
@@ -11,6 +11,29 @@ using namespace std;
 void Day_04(ifstream& InputFile)
 {
 	// read input line per line
-	vector<string> ListOfInputs{ istream_iterator<string>{InputFile},{} };
+	//vector<string> ListOfInputs{ istream_iterator<string>{InputFile},{} };
+
+	int PassRangeBegin = 136818;
+	int PassRangeEnd = 685979;
+
+	vector<int> FirstPassword;
+	string CheckPassword = to_string(PassRangeBegin);
+
+	// convert strings to containers of integers
+	int Counter = 0;
+	for (auto Candidate = PassRangeBegin; Candidate <= PassRangeEnd; Candidate++)
+	{
+		string StringVersion = to_string(Candidate);
+		vector<int> VectorCandidate = {};
+		for (auto digit : StringVersion) VectorCandidate.push_back(digit-48);
+		if (!prev_permutation(VectorCandidate.begin(), VectorCandidate.end())
+			&&
+		adjacent_find(VectorCandidate.begin(),VectorCandidate.end()) != VectorCandidate.end())
+			 Counter++;
+
+	}
+
+	cout << Counter;
+
 
 }
