912f135366493ac2e826b8515102255751329a71
Maja Gacesa <maja.gacesa@gmail.com>
2019-12-04 14:24:45 +0100

    [day04] clean up code

--- 04th_of_2019.cpp
+++ 04th_of_2019.cpp
@@ -10,49 +10,50 @@ using namespace std;
 
 void Day_04(ifstream& InputFile)
 {
-	// read input line per line
-	//vector<string> ListOfInputs{ istream_iterator<string>{InputFile},{} };
+	//read input (stream of chars is read into a string)
+	string RangeOfPasswords{ istream_iterator<char>{InputFile},{} };
+	
+	// parse input
+	int pos = RangeOfPasswords.find("-");
+	int PassRangeBegin = stoi(RangeOfPasswords.substr(0,pos));
+	int PassRangeEnd = stoi(RangeOfPasswords.substr(pos+1));
 
-	int PassRangeBegin = 136818;
-	int PassRangeEnd = 685979;
-
-	vector<int> FirstPassword;
-	string CheckPassword = to_string(PassRangeBegin);
+	int Counter = 0; // solution to part 1
+	int CounterExtraRule = 0; // solution to part 2
 
 	// convert strings to containers of integers
-	int Counter = 0;
-	int CounterExtraRule = 0;
-	for (auto Candidate = PassRangeBegin; Candidate <= PassRangeEnd; Candidate++)
+	for ( auto Candidate = PassRangeBegin; Candidate <= PassRangeEnd; Candidate++ )
 	{
 		string StringVersion = to_string(Candidate);
 		vector<int> VectorCandidate = {};
 		for (auto digit : StringVersion) VectorCandidate.push_back(digit-48);
+		// rule 1: the digits must be in ascending order
+		// rule 2: there must be some c
 		if (!prev_permutation(VectorCandidate.begin(), VectorCandidate.end())
 			&&
 			adjacent_find(VectorCandidate.begin(), VectorCandidate.end()) != VectorCandidate.end())
 		{
 			Counter++;
+			// part 2: extra rule
 			vector<int> UniqueDigits = VectorCandidate;
 			auto it = unique(UniqueDigits.begin(), UniqueDigits.end());
 			UniqueDigits.resize(it - UniqueDigits.begin());
+
 			for (auto UniqueDigit : UniqueDigits)
 			{
-				if (count_if(VectorCandidate.begin(), VectorCandidate.end(), [UniqueDigit](int CandidateDigit)
+				if (count_if(VectorCandidate.begin(), VectorCandidate.end(),
+					[UniqueDigit](int CandidateDigit)
 				{return CandidateDigit == UniqueDigit; }) == 2)
+
 				{
 					CounterExtraRule++;
 					break;
 				}
-				
 			}
-			
 		}
-
 	}
 
-	cout << Counter << "\n";
-	cout << CounterExtraRule << "\n";
-
-
+	cout << "The number of passwords satisfying the initial rule (part 1) is: " << Counter << "...\n";
+	cout << "...of which " << CounterExtraRule << " satisty the extra rule!\n";
 
 }
