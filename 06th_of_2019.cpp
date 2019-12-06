b77c28ff6a3fb56d149cb10852733952bd32f62b
Maja Gacesa <maja.gacesa@gmail.com>
2019-12-06 07:58:22 +0100

    [day06] read input into a map of orbit definitions

--- 06th_of_2019.cpp
+++ 06th_of_2019.cpp
@@ -5,6 +5,7 @@
 #include <iterator>
 #include <algorithm>
 #include <set>
+#include <map>
 #include <numeric>
 #include "DayHeaders.h"
 
@@ -13,7 +14,16 @@ using namespace std;
 
 void Day_06(ifstream& InputFile)
 {
+	map <string, string> OrbitDefintions;
 	// parse input
 	vector<string> ListOfInputs{ istream_iterator<string>{InputFile},{} };
+	for (auto OneOrbit : ListOfInputs)
+	{
+		int pos_separator = OneOrbit.find(')');
+		string Center = OneOrbit.substr(0, pos_separator);
+		string Planet = OneOrbit.substr(pos_separator+1);
+		OrbitDefintions[Center] = Planet;
+
+	}
 
 }
