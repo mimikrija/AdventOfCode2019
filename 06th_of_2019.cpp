7018622e686be10bc4b83292f77f208c51bea08b
Maja Gacesa <maja.gacesa@gmail.com>
2019-12-06 18:12:05 +0100

    [day06] part 2

--- 06th_of_2019.cpp
+++ 06th_of_2019.cpp
@@ -27,9 +27,36 @@ void Day_06(ifstream& InputFile)
 		OrbitCounts[Planet] = 0;
 	}
 
-
+	// part 2:
+	int YouPath = 0;
+	int SantaPath = 0;
+	int DistanceYouToSanta = 0;
+	string LookingFor = "YOU";
+	vector<string> YouChain = {};
+	vector<string> SantaChain = {};
+	while ( OrbitDefintions.find(LookingFor) != OrbitDefintions.end())
+	{
+		YouPath++;
+		LookingFor = OrbitDefintions.find(LookingFor)->second;
+		YouChain.push_back(LookingFor);
+	}
+	LookingFor = "SAN";
+	while (OrbitDefintions.find(LookingFor) != OrbitDefintions.end())
+	{
+		YouPath++;
+		LookingFor = OrbitDefintions.find(LookingFor)->second;
+		SantaChain.push_back(LookingFor);
+	}
 	
+	sort(SantaChain.begin(), SantaChain.end());
+	sort(YouChain.begin(), YouChain.end());
+	vector<string> ToTheMergebase(SantaChain.size()+YouChain.size());
+	auto diff = set_symmetric_difference(SantaChain.begin(), SantaChain.end(), YouChain.begin(), YouChain.end(),ToTheMergebase.begin());
+	ToTheMergebase.resize(diff - ToTheMergebase.begin());
+	DistanceYouToSanta = ToTheMergebase.size();
+
 	string CurrentCenter = "COM";
+
 	vector<string> SoFarInTheChain = { CurrentCenter };
 	while (OrbitDefintions.size() != 0)
 	{
@@ -55,4 +82,5 @@ void Day_06(ifstream& InputFile)
 
 	cout << "The total count of orbits in this system is: " << TotalOrbitalCount << "!\n";
 // Part 1: 273985
+	cout << "The total count of orbits between you and Santa is: " << DistanceYouToSanta << "!\n";
 }
