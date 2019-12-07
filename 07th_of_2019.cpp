77c5e48929708e9dd2aebebee47d6cbdc7ab8e87
Maja Gacesa <maja.gacesa@gmail.com>
2019-12-07 10:59:03 +0100

    [day07] part 1 solution
    loop that runs until all permutations of input signals are fed to the program,
    which starts all over again for each combination. needs to be cleaned up.

--- 07th_of_2019.cpp
+++ 07th_of_2019.cpp
@@ -9,6 +9,8 @@
 #include "DayHeaders.h"
 
 
+
+
 using namespace std;
 
 void Day_07(ifstream& InputFile)
@@ -28,91 +30,115 @@ void Day_07(ifstream& InputFile)
 			CodeList.push_back(stoi(value));
 		}
 	}
+	
+	vector<int> CleanCode = CodeList;
+	vector<int> AllResults;
+	vector<int> AmplifierInputs = { 0,1,2,3,4 };
 
-
-	int pos = 0;
-	cout << "Give me the input: ";
-	int UserInput;//we provide that to the program
-	cin >> UserInput;
-	while ( pos < CodeList.size())
+	while (next_permutation(AmplifierInputs.begin(),AmplifierInputs.end()))
 	{
-		int result;
-		int code = GetCode(CodeList.at(pos));
-		if (code == 99)
+
+		int LastOutput = 0;
+		for (int i = 0; i < 5; i++)
 		{
-			pos++;
-			break;
+			int inputcount = 0;
+
+			int UserInput = AmplifierInputs.at(i);//we provide that to the program
+			//cin >> UserInput;
+			//cout << "Amplifier number " << i << " running. Phase setting: " << UserInput << "\n";
+			int pos = 0;
+
+			CodeList = CleanCode;
+			while (pos < CodeList.size())
+			{
+				int result;
+				int code = GetCode(CodeList.at(pos));
+				if (code == 99)
+				{
+					pos++;
+					break;
+				}
+
+				int ParametersInCommand = NumberOfParameters(code);
+				bool PositionModeFirst = !GetFirstParameterMode(CodeList.at(pos));
+				bool PositionModeSecond, PositionModeThird;
+				int FirstParameter, SecondParameter, ThirdParameter;
+				int WritePosition = 0;
+
+				// default
+				if (ParametersInCommand >= 1)
+				{
+					PositionModeFirst ?
+						FirstParameter = CodeList.at(CodeList.at(pos + 1))
+						:
+						FirstParameter = CodeList.at(pos + 1);
+				}
+				if (ParametersInCommand >= 2)
+				{
+					PositionModeSecond = !GetSecondParameterMode(CodeList.at(pos));
+					PositionModeSecond ?
+						SecondParameter = CodeList.at(CodeList.at(pos + 2))
+						:
+						SecondParameter = CodeList.at(pos + 2);
+				}
+				if (ParametersInCommand >= 3)
+				{
+					PositionModeThird = !GetThirdParameterMode(CodeList.at(pos));
+					PositionModeThird ?
+						ThirdParameter = CodeList.at(CodeList.at(pos + 3))
+						:
+						ThirdParameter = CodeList.at(pos + 3);
+				}
+
+
+				switch (code)
+				{
+				case 1: case 2: case 7: case 8:
+					result = GetResult(code, FirstParameter, SecondParameter);
+					WritePosition = ThirdParameter;
+					pos += ParametersInCommand + 1;
+					break;
+
+				case 3:
+					WritePosition = FirstParameter;
+					if (inputcount == 0)
+					{
+						result = UserInput;
+						inputcount++;
+					}
+					else result = LastOutput;
+					pos += ParametersInCommand + 1;
+					break;
+
+				case 4:
+					result = FirstParameter;
+					//cout << "Output produced " << result << "\n";
+					pos += ParametersInCommand + 1;
+					LastOutput = result;
+					break;
+
+				case 5:
+					FirstParameter != 0 ? pos = SecondParameter : pos += ParametersInCommand + 1;
+					break;
+
+				case 6:
+					FirstParameter == 0 ? pos = SecondParameter : pos += ParametersInCommand + 1;
+					break;
+
+				default:
+					break;
+				}
+
+				// exclude codes which do not write
+				if (!(code == 4 || code == 5 || code == 6))
+					CodeList.at(WritePosition) = result;
+
+			}
 		}
-
-		int ParametersInCommand = NumberOfParameters(code);
-		bool PositionModeFirst = !GetFirstParameterMode(CodeList.at(pos));
-		bool PositionModeSecond, PositionModeThird;
-		int FirstParameter, SecondParameter, ThirdParameter;
-		int WritePosition = 0;
-		
-		// default
-		if (ParametersInCommand >= 1)
-		{
-			PositionModeFirst ?
-				FirstParameter = CodeList.at(CodeList.at(pos + 1))
-				:
-				FirstParameter = CodeList.at(pos + 1);
-		}
-		if (ParametersInCommand >= 2)
-		{
-			PositionModeSecond = !GetSecondParameterMode(CodeList.at(pos));
-			PositionModeSecond ?
-				SecondParameter = CodeList.at(CodeList.at(pos + 2))
-				:
-				SecondParameter = CodeList.at(pos + 2);
-		}
-		if (ParametersInCommand >= 3)
-		{
-			PositionModeThird = !GetThirdParameterMode(CodeList.at(pos));
-			PositionModeThird ?
-				ThirdParameter = CodeList.at(CodeList.at(pos + 3))
-				:
-				ThirdParameter = CodeList.at(pos + 3);
-		}
-
-
-		switch (code)
-		{
-		case 1: case 2: case 7: case 8:
-			result = GetResult(code, FirstParameter, SecondParameter);
-			WritePosition = ThirdParameter;
-			pos += ParametersInCommand + 1;
-			break;
-
-		case 3:
-			WritePosition = FirstParameter;
-			result = UserInput;
-			pos += ParametersInCommand + 1;
-			break;
-
-		case 4:
-			result = FirstParameter;
-			cout << "Output produced " << result << "\n";
-			pos += ParametersInCommand + 1;
-			break;
-
-		case 5:
-			FirstParameter != 0 ? pos = SecondParameter : pos+= ParametersInCommand + 1;
-			break;
-
-		case 6:
-			FirstParameter == 0 ? pos = SecondParameter : pos += ParametersInCommand + 1;
-			break;
-
-		default:
-			break;
-		}
-
-		// exclude codes which do not write
-		if ( !( code == 4 || code ==5 || code ==6))
-			CodeList.at(WritePosition) = result;
-		
+		AllResults.push_back(LastOutput);
 	}
 
+	cout << "Part 1 solution is " << *max_element(AllResults.begin(), AllResults.end()) << "!\n";
 
+	// Part 1: 65464
 }
