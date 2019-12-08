d0cfa97052bf8e6e2324e1d6374a39034b39d8c3
Maja Gacesa <maja.gacesa@gmail.com>
2019-12-08 10:12:34 +0100

    [day08] part 1

--- 08th_of_2019.cpp
+++ 08th_of_2019.cpp
@@ -15,4 +15,25 @@ void Day_08(ifstream& InputFile)
 	{
 		Pixels.push_back(Digit);
 	}
+
+// The image you received is 25 pixels wide and 6 pixels tall.
+	vector<vector<char>> Layers;
+	vector<int> zerocounts;
+	for (int LayerCount = 0; LayerCount < Pixels.size() / (25 * 6); LayerCount++)
+	{
+		vector<char> OneLayer = {};
+		for (int PixelsInLayer = 0; PixelsInLayer < 25 * 6; PixelsInLayer++)
+		{
+			OneLayer.push_back(Pixels.at(LayerCount*25*6+PixelsInLayer));
+		}
+		Layers.push_back(OneLayer);
+		zerocounts.push_back(count(OneLayer.begin(),OneLayer.end(),'0'));
+	}
+
+	int TheLayer = distance(zerocounts.begin(), min_element(zerocounts.begin(), zerocounts.end())) ;
+	int countones = count((Layers.at(TheLayer)).begin(), (Layers.at(TheLayer)).end(),'1');
+	int counttwos = count((Layers.at(TheLayer)).begin(), (Layers.at(TheLayer)).end(),'2');
+	
+	cout << "Part one solution is: " << countones * counttwos << "!\n";
+	// 1524 too low
 }
