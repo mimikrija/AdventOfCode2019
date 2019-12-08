248025974d065773b23d52c7865511ace7e42397
Maja Gacesa <maja.gacesa@gmail.com>
2019-12-08 22:16:53 +0100

    [day07] this works, provides outputs for several runs, but
    I do not get the expected result. in this implementation, once we have output
    the programm is "paused", the output is passed to the next amplifier as an
    input. When the amplifier is reached the next time, the program continues
    where it was paused in the previous instance. The program is not reset during
    calculation. Therefore, these two values (input vector and position) are
    passed as reference.
    
    If the run is marked as initial, the first input instruction sent to the
    amplifier is its phase signal, and otherwise it is the input signal. HA!
    
    I GOT IT. The input is called more than once during a single execution, and
    I did not account for that with the OptionalInput check in code case == 4.

--- 07th_of_2019.cpp
+++ 07th_of_2019.cpp
@@ -9,9 +9,118 @@
 #include "DayHeaders.h"
 
 
-
-
 using namespace std;
+
+int OptCode(vector<int>& Program, int DefaultInput,  bool &IsFinished, int &pos,int OptionalInput = -1)
+{
+	// Program is taken by reference because it is changed and needs to remain that way
+	// even after this runs
+
+	// Default Input is (in this case) the output of the previous amplifier, but it can
+	// be used for any default input
+
+	// OptionalInput is used only when provided - in this case only in the initial run
+	// when we provide the phase code to each amplifier.
+	// in this case, the phase codes are 0-9 - so -1 is a safe choice for 
+	// checking whether to use this input or not
+
+	// IsFinished, also by reference serves to communicate to the main program whether that
+	// amplifier is finished or not (hit code 99)
+	int Output = 0;
+	
+
+	//int pos = continuefrom;
+
+	
+	while (pos < Program.size())
+	{
+		int result;
+		int code = GetCode(Program.at(pos));
+		if (code == 99)
+		{
+			pos++; // useless now
+			IsFinished = true;
+			return Output; //not that it matters?
+		}
+
+		int ParametersInCommand = NumberOfParameters(code);
+		bool PositionModeFirst = !GetFirstParameterMode(Program.at(pos));
+		bool PositionModeSecond, PositionModeThird;
+		int FirstParameter, SecondParameter, ThirdParameter;
+		int WritePosition = 0;
+
+		// default
+		if (ParametersInCommand >= 1)
+		{
+			PositionModeFirst ?
+				FirstParameter = Program.at(Program.at(pos + 1))
+				:
+				FirstParameter = Program.at(pos + 1);
+		}
+		if (ParametersInCommand >= 2)
+		{
+			PositionModeSecond = !GetSecondParameterMode(Program.at(pos));
+			PositionModeSecond ?
+				SecondParameter = Program.at(Program.at(pos + 2))
+				:
+				SecondParameter = Program.at(pos + 2);
+		}
+		if (ParametersInCommand >= 3)
+		{
+			PositionModeThird = !GetThirdParameterMode(Program.at(pos));
+			PositionModeThird ?
+				ThirdParameter = Program.at(Program.at(pos + 3))
+				:
+				ThirdParameter = Program.at(pos + 3);
+		}
+
+
+		switch (code)
+		{
+		case 1: case 2: case 7: case 8:
+			result = GetResult(code, FirstParameter, SecondParameter);
+			WritePosition = ThirdParameter;
+			pos += ParametersInCommand + 1;
+			break;
+
+		case 3:
+			WritePosition = FirstParameter;
+			OptionalInput >= 0 ? result = OptionalInput : result = DefaultInput;
+			pos += ParametersInCommand + 1;
+			break;
+
+		case 4:
+			result = FirstParameter;
+			//cout << "Output produced " << result << "\n";
+			pos += ParametersInCommand + 1;
+			Output = result;
+			break;
+
+		case 5:
+			FirstParameter != 0 ? pos = SecondParameter : pos += ParametersInCommand + 1;
+			break;
+
+		case 6:
+			FirstParameter == 0 ? pos = SecondParameter : pos += ParametersInCommand + 1;
+			break;
+
+		default:
+			break;
+		}
+
+		// exclude codes which do not write
+		if (!(code == 4 || code == 5 || code == 6))
+			Program.at(WritePosition) = result;
+
+		if (code == 4) return Output;
+
+		// once we have output proceed to the next amplifier
+	}
+	
+	
+}
+
+
 
 void Day_07(ifstream& InputFile)
 {
@@ -33,112 +142,45 @@ void Day_07(ifstream& InputFile)
 	
 	vector<int> CleanCode = CodeList;
 	vector<int> AllResults;
-	vector<int> AmplifierInputs = { 0,1,2,3,4 };
+	vector<int> AmplifierInputs = { 9,7,8,5,6 };
+	vector<vector<int>> AmplifierOutputs(5);
+	vector<vector<int>> AmplifierPrograms{ 5,CodeList };
+	vector<int> ContinueFrom(5);
 
-	while (next_permutation(AmplifierInputs.begin(),AmplifierInputs.end()))
+	bool InitialRun = true;
+	bool IsFinished = false;
+	int i = 0;
+	int Input = 0; // this is the initial input for A
+	int Output;
+	while (true)
 	{
-
-		int LastOutput = 0;
-		for (int i = 0; i < 5; i++)
+		if (i == 0 && InitialRun) Input = 0;
+		else
 		{
-			int inputcount = 0;
-
-			int UserInput = AmplifierInputs.at(i);//we provide that to the program
-			//cin >> UserInput;
-			//cout << "Amplifier number " << i << " running. Phase setting: " << UserInput << "\n";
-			int pos = 0;
-
-			CodeList = CleanCode;
-			while (pos < CodeList.size())
-			{
-				int result;
-				int code = GetCode(CodeList.at(pos));
-				if (code == 99)
-				{
-					pos++;
-					break;
-				}
-
-				int ParametersInCommand = NumberOfParameters(code);
-				bool PositionModeFirst = !GetFirstParameterMode(CodeList.at(pos));
-				bool PositionModeSecond, PositionModeThird;
-				int FirstParameter, SecondParameter, ThirdParameter;
-				int WritePosition = 0;
-
-				// default
-				if (ParametersInCommand >= 1)
-				{
-					PositionModeFirst ?
-						FirstParameter = CodeList.at(CodeList.at(pos + 1))
-						:
-						FirstParameter = CodeList.at(pos + 1);
-				}
-				if (ParametersInCommand >= 2)
-				{
-					PositionModeSecond = !GetSecondParameterMode(CodeList.at(pos));
-					PositionModeSecond ?
-						SecondParameter = CodeList.at(CodeList.at(pos + 2))
-						:
-						SecondParameter = CodeList.at(pos + 2);
-				}
-				if (ParametersInCommand >= 3)
-				{
-					PositionModeThird = !GetThirdParameterMode(CodeList.at(pos));
-					PositionModeThird ?
-						ThirdParameter = CodeList.at(CodeList.at(pos + 3))
-						:
-						ThirdParameter = CodeList.at(pos + 3);
-				}
-
-
-				switch (code)
-				{
-				case 1: case 2: case 7: case 8:
-					result = GetResult(code, FirstParameter, SecondParameter);
-					WritePosition = ThirdParameter;
-					pos += ParametersInCommand + 1;
-					break;
-
-				case 3:
-					WritePosition = FirstParameter;
-					if (inputcount == 0)
-					{
-						result = UserInput;
-						inputcount++;
-					}
-					else result = LastOutput;
-					pos += ParametersInCommand + 1;
-					break;
-
-				case 4:
-					result = FirstParameter;
-					//cout << "Output produced " << result << "\n";
-					pos += ParametersInCommand + 1;
-					LastOutput = result;
-					break;
-
-				case 5:
-					FirstParameter != 0 ? pos = SecondParameter : pos += ParametersInCommand + 1;
-					break;
-
-				case 6:
-					FirstParameter == 0 ? pos = SecondParameter : pos += ParametersInCommand + 1;
-					break;
-
-				default:
-					break;
-				}
-
-				// exclude codes which do not write
-				if (!(code == 4 || code == 5 || code == 6))
-					CodeList.at(WritePosition) = result;
-
-			}
+			i != 0 ? Input = (AmplifierOutputs.at(i - 1)).back() : Input = (AmplifierOutputs.at(4)).back();
 		}
-		AllResults.push_back(LastOutput);
-	}
+		if (InitialRun) // call with extra parameter as input
+		{
+			Output = OptCode(AmplifierPrograms.at(i), Input, IsFinished, ContinueFrom.at(i),AmplifierInputs.at(i));
+			if (i==4) InitialRun = false;
+		}
+		else // call with default parameters
+		{
+			Output = OptCode(AmplifierPrograms.at(i), Input, IsFinished,ContinueFrom.at(i));
+		}
+		if (!IsFinished) AmplifierOutputs.at(i).push_back(Output);
+		if (i != 4 ) IsFinished = false;
+		if (IsFinished) break;
+		
+		i == 4 ? i = 0 : i++;
 
-	cout << "Part 1 solution is " << *max_element(AllResults.begin(), AllResults.end()) << "!\n";
+		
+	}
+		//AllResults.push_back(LastOutput);
+	
+	int test = accumulate(AmplifierOutputs.at(4).begin(), AmplifierOutputs.at(4).end(), 0);
+
+	cout << "Part 2 solution is " << *max_element(AmplifierOutputs.at(4).begin(),AmplifierOutputs.at(4).end()) << "!\n";
 
 	// Part 1: 65464
 }
