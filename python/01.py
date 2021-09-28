0010e72e5da955b1455fac5af5b577fb55c21b01
Maja Gacesa <maja.gacesa@gmail.com>
2021-09-28 18:03:46 +0200

    [day01] party 2 first attempt

--- python/01.py
+++ python/01.py
@@ -5,9 +5,28 @@ from santas_little_helpers import *
 def calculate_fuel(in_mass):
     return in_mass//3-2
 
+def recursive_fuel(in_mass):
+    mass = in_mass
+    fuel = calculate_fuel(mass)
+    if fuel <= 0:
+        return mass
+    else:
+        mass += recursive_fuel(fuel)
 
-masses = map(int, get_input('inputs/01'))
+def recursive_fuel(in_mass):
+    mass = in_mass
+    fuel = 0
+    while True:
+        mass = calculate_fuel(mass)
+        if mass > 0:
+            fuel += mass
+        else:
+            return fuel
+
+
+masses = list(map(int, get_input('inputs/01')))
 
 party_1 = sum(calculate_fuel(mass) for mass in masses)
+party_2 = sum(recursive_fuel(mass) for mass in masses)
 
-print_solutions(party_1)
+print_solutions(party_1, party_2)
