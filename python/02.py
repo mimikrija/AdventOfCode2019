1cf02f67c5d39ff37a3a0c499d6d66bec114b38f
Maja Gacesa <maja.gacesa@gmail.com>
2021-10-07 23:42:52 +0200

    [day02] objectify! cool, but pt 2 now doesnt work

--- python/02.py
+++ python/02.py
@@ -2,68 +2,64 @@
 
 from santas_little_helpers import *
 from itertools import product
+from functools import reduce
+from operator import mul
+
+class Program:
+    VALUES_IN_INSTRUCTION = {
+        1: 3,
+        2: 3,
+        99: 0,
+    }
+
+    def __init__(self, input=[]):
+        self.memory = input
+        self.address = 0
+
+    def get(self, address):
+        return self.memory[address]
+    
+    def set(self, address, value):
+        self.memory[address] = value
+
+    def get_parameters(self, address):
+        num_of_parameters = self.VALUES_IN_INSTRUCTION[self.memory[address]]
+        return self.memory[address+1:address+num_of_parameters+1]
+
+    def operation(self, opcode, arguments):
+        if opcode == 1:
+            return sum(arguments)
+        if opcode == 2:
+            return reduce(mul, arguments)
+
+    def run(self):
+        current_address = 0
+        while True:
+            opcode = self.memory[current_address]
+            if opcode == 99:
+                return self
+            parameters = self.get_parameters(current_address)
+            arguments = [self.memory[num] for num in parameters[:-1]]
+            result_position = parameters[-1]
+            self.set(result_position, self.operation(opcode, arguments))
+            current_address += len(parameters) + 1
 
 
-def operation(opcode, arguments=[]):
-    first, second = arguments
-    if opcode == 1:
-        return first + second
-    if opcode == 2:
-        return first * second
-    if opcode == 99:
-        return
+original_program = list(map(int, get_input('inputs/02',',')))
 
-def parameters_count(opcode):
-    if opcode == 1 or opcode == 2:
-        return 3
-    if opcode == 99:
-        return 0
-    else:
-        raise ValueError('unknown opcode encountered!')
+program_part_1 = Program(original_program)
+program_part_1.set(1, 12)
+program_part_1.set(2, 2)
+party_1 = program_part_1.run().get(0)
 
-def reset_memory(prog, address, value):
-    prog[address] = value
-    return prog
+print_solutions(party_1)
 
-def get_parameters(prog, opcode, address):
-    if opcode == 99:
-        return None, None
-    param_positions = prog[address+1:address+parameters_count(opcode)+1]
-    parameters = [prog[pos] for pos in param_positions[:-1]]
-    return parameters, param_positions[-1]
-
-def intcode(prog, address=0):
-    opcode = prog[address]
-    arguments, result_position = get_parameters(prog, opcode, address)
-    if arguments:
-        return reset_memory(prog, result_position, operation(opcode, arguments)), address + parameters_count(opcode) + 1
-    # no args: code 99 we are done with the program
-    return prog, None
-
-def run_program(program):
-    position = 0
-    while True:
-        program, position = intcode(program, position)
-        if position == None:
-            return program
-
-
-
-program = list(map(int, get_input('inputs/02',',')))
-program_part_1 = list(program)
-# To do this, before running the program, replace position 1
-# with the value 12 and replace position 2 with the value 2.
-program_part_1 = reset_memory(program_part_1, 1, 12)
-program_part_1 = reset_memory(program_part_1, 2, 2)
-
-party_1 = run_program(program_part_1)[0]
-
-
+# this now doesn't work KeyError: 3516593
 for noun, verb in product(range(0, 100), repeat=2):
-    program_part_2 = list(program)
-    program_part_2 = reset_memory(program_part_2, 1, noun)
-    program_part_2 = reset_memory(program_part_2, 2, verb)
-    if run_program(program_part_2)[0] == 19690720:
+    program_part_2 = Program(original_program)
+    program_part_2.set(1, noun)
+    program_part_2.set(2, verb)
+    if program_part_2.run().get(0) == 19690720:
         party_2 = 100 * noun + verb
         break
 
