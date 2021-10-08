b753834f24a31051cca9b6f788ef424f855a6b60
Maja Gacesa <maja.gacesa@gmail.com>
2021-10-08 22:55:32 +0200

    [day03] switch from sets to lists to track paths
    use index to find the first occurrence of an intersection in a path

--- python/03.py
+++ python/03.py
@@ -13,18 +13,22 @@ def manhattan_distance(coordinate):
     return int(abs(coordinate.real) + abs(coordinate.imag))
 
 def generate_path(wire):
-    path = set()
+    path = [0+0j]
     current = 0+0j
     for direction, amount in wire:
         for _ in range(amount):
             current += MOVE[direction]
-            path.add(current)
+            path.append(current)
     return path
 
 wires = [[(c[0], int(c[1:])) for c in line.split(',')] for line in get_input('inputs/input03')]
 
 path_1, path_2 = (generate_path(wire) for wire in wires)
 
-intersections = path_1 & path_2
+intersections = set(path_1) & set(path_2)
+intersections.remove(0+0j)
+
 party_1 = min(manhattan_distance(intersection) for intersection in intersections)
-print_solutions(party_1)
+party_2 = min(path_1.index(intersection) + path_2.index(intersection) for intersection in intersections)
+
+print_solutions(party_1, party_2)
