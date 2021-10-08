88f71ea98e27da4c98bd443215cf114daabe97a3
Maja Gacesa <maja.gacesa@gmail.com>
2021-10-08 23:23:05 +0200

    [day04] cleanup

--- python/04.py
+++ python/04.py
@@ -4,6 +4,7 @@ from santas_little_helpers import *
 from collections import Counter
 
 def is_valid(password):
+    password = str(password)
     if len(password) != 6:
         return False
     if any(first > second for first, second in zip(password[:-1], password[1:])):
@@ -13,12 +14,12 @@ def is_valid(password):
     return False
 
 def new_rule(password):
+    password = str(password)
     return 2 in Counter(password).values()
 
 low, high = map(int, get_input('inputs/input04','-'))
 
 
-party_1 = sum(is_valid(str(password)) for password in range(low, high+1))
-party_2 = sum(is_valid(str(password)) and new_rule(str(password)) for password in range(low, high+1))
+party_1 = sum(is_valid(password) for password in range(low, high+1))
+party_2 = sum(is_valid(password) and new_rule(password) for password in range(low, high+1))
 print_solutions(party_1, party_2)
-
