74d6c46ef372b22b7e84320991ec90b07320c4d8
Maja Gacesa <maja.gacesa@gmail.com>
2021-10-08 09:30:24 +0200

    [day02] make it more "objective" do not reurn self from run
    thanks @narimiran

--- python/02.py
+++ python/02.py
@@ -37,7 +37,7 @@ class Program:
         while True:
             opcode = self.memory[current_address]
             if opcode == 99:
-                return self
+                return
             parameters = self.get_parameters(current_address)
             arguments = [self.memory[num] for num in parameters[:-1]]
             result_position = parameters[-1]
@@ -50,13 +50,15 @@ original_program = list(map(int, get_input('inputs/02',',')))
 program_part_1 = Program(original_program)
 program_part_1.set(1, 12)
 program_part_1.set(2, 2)
-party_1 = program_part_1.run().get(0)
+program_part_1.run()
+party_1 = program_part_1.get(0)
 
 for noun, verb in product(range(0, 100), repeat=2):
     program_part_2 = Program(original_program)
     program_part_2.set(1, noun)
     program_part_2.set(2, verb)
-    if program_part_2.run().get(0) == 19690720:
+    program_part_2.run()
+    if program_part_2.get(0) == 19690720:
         party_2 = 100 * noun + verb
         break
 
