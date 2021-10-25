1b47f44e300fbb37b182d3c7c4f0876b1bb03d3d
Maja Gacesa <maja.gacesa@gmail.com>
2021-10-25 23:05:12 +0200

    [day05] fix opcode 4

--- python/intcode.py
+++ python/intcode.py
@@ -6,6 +6,7 @@ class Program:
         1: 3,
         2: 3,
         3: 1,
+        4: 1,
         99: 0,
     }
 
@@ -24,7 +25,6 @@ class Program:
     def get_argument(self, arg_pos):
         modes = self.get(self.instr_pointer) // 100
         immediate_mode = (modes // 10**(arg_pos-1)) % 10
-        print(arg_pos, immediate_mode)
         parameter = self.memory[self.instr_pointer+arg_pos]
         if immediate_mode:
             return parameter
@@ -33,7 +33,6 @@ class Program:
 
     def binary_operation(self, opcode):
         first, second = [self.get_argument(pos) for pos in {1,2}]
-        print(first, second)
         if opcode == 1:
             return first + second
         if opcode == 2:
@@ -42,7 +41,6 @@ class Program:
     def run(self):
         while True:
             opcode = self.get(self.instr_pointer) % 100
-            print(opcode, self.memory)
             if opcode == 99:
                 return
             if opcode <= 2:
@@ -51,8 +49,9 @@ class Program:
                 result_value = self.input_value
             if opcode == 4:
                 self.output.append(self.get_argument(1))
-                continue
-            result_position = self.get(self.instr_pointer+self.VALUES_IN_INSTRUCTION[opcode])
-            self.set(result_position, result_value)
+            if opcode <= 3:
+                result_position = self.get(self.instr_pointer+self.VALUES_IN_INSTRUCTION[opcode])
+                self.set(result_position, result_value)
+            
             # increment pointer
             self.instr_pointer += self.VALUES_IN_INSTRUCTION[opcode] + 1
