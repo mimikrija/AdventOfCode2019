3d3de84f2ed49e278694019affd2990ef76683d3
Maja Gacesa <maja.gacesa@gmail.com>
2021-10-26 23:14:58 +0200

    [day07] party 1 with permutations

--- python/07.py
+++ python/07.py
@@ -1,6 +1,20 @@
 # Day 7: Amplification Circuit
 
 from santas_little_helpers import *
+from intcode import Program
+from itertools import permutations
+
+def run_amplifiers(in_program, phases):
+    start=0
+    for phase in phases:
+        amplifier = Program(in_program,[phase, start])
+        amplifier.run()
+        start = amplifier.output[0]
+    return start
+
 
 original_program = list(map(int, get_input('inputs/input07',',')))
 
+party_1 = max(run_amplifiers(original_program, combo) for combo in permutations(range(5)))
+
+print_solutions(party_1)
