62bb4f0a2d410de2bbbf24407c9eaebe52c31a17
Maja Gacesa <maja.gacesa@gmail.com>
2021-10-28 17:10:35 +0200

    [day07] party 2

--- python/07.py
+++ python/07.py
@@ -12,9 +12,25 @@ def run_amplifiers(in_program, phases):
         start = amplifier.output[0]
     return start
 
+def amplifier_feedback_loop(in_program, phases):
+    # initialize amplifiers
+    amplifiers = [Program(in_program,[phase]) for phase in phases]
+    for amp in amplifiers:
+        amp.feedback_loop_mode = True
+    # initialize input for first amplifier
+    result = 0
+
+    while result is not None:
+        for amp in amplifiers:
+            amp.inputs.append(result)
+            result = amp.run()
+    # last solution of the last amplifier is the solution
+    return amplifiers[-1].output[-1]
+
 
 original_program = list(map(int, get_input('inputs/input07',',')))
 
 party_1 = max(run_amplifiers(original_program, combo) for combo in permutations(range(5)))
+party_2 = max(amplifier_feedback_loop(original_program, combo) for combo in permutations(range(5,10)))
 
-print_solutions(party_1)
+print_solutions(party_1, party_2)
