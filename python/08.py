f0cbd60142c77ec38ff177bcc4f07841ecb74ff8
Maja Gacesa <maja.gacesa@gmail.com>
2021-11-02 12:46:23 +0100

    [day08] find first visible color per layer
    - zip(*layers) to 'transpose' the matrix and go through columns instead
    of rows
    - use next to get only the first value which is not '2' (transparent)

--- python/08.py
+++ python/08.py
@@ -13,3 +13,13 @@ fewest_zeros_layer = min(layers, key=lambda x: x.count('0'))
 party_1 = fewest_zeros_layer.count('1') * fewest_zeros_layer.count('2')
 
 print_solutions(party_1)
+
+# part 2
+paint = lambda x: 'X' if x=='1' else ' '
+color_image = [paint(next(c for c in colors if c != '2')) for colors in zip(*layers)]
+
+final_image = [''.join(c for c in color_image[pos:pos+width]) for pos in range(0, len(color_image), width)]
+
+print('Part 2 solution is: \n')
+for line in final_image:
+    print(line)
