484dd4c2cb9f1893139937c21165c4944b9547b2
Maja Gacesa <maja.gacesa@gmail.com>
2021-11-04 20:25:24 +0100

    [day11] fix part 1 logic although it worked
    I was only adding white fields to visited_at_least_once, but I should
    have been adding all positions - so I guess I was lucky.

--- python/11.py
+++ python/11.py
@@ -6,8 +6,8 @@ from intcode import Program
 def get_color_and_rotation(robot_code, input_value):
     robot_code.inputs.append(input_value)
     try:
-        color, rotation = (robot_code.run() for _ in range(2))
-        return color, rotation
+        is_white, rotation = (robot_code.run() for _ in range(2))
+        return is_white, rotation
     except:
         return None, None
 
@@ -16,21 +16,21 @@ def paint_the_panel(robot_instructions, first_white=False):
     robot_code = Program(robot_instructions)
     robot_code.feedback_loop_mode = True
     position = 0+0j
-    positions = set()
+    white_fields = set()
     if first_white:
-        positions.add(position)
-    direction = 0+1j
-    painted_at_least_once = set()
+        white_fields.add(position)
+    direction = 0-1j
+    all_positions = set()
     while True:
-        current_color = position in positions
-        color, rotation = get_color_and_rotation(robot_code, current_color)
-        if color == None and rotation == None:
-            return positions, len(painted_at_least_once)
-        if color:
-            positions.add(position)
-            painted_at_least_once.add(position)
+        current_color = position in white_fields
+        is_white, rotation = get_color_and_rotation(robot_code, current_color)
+        if is_white == None and rotation == None:
+            return all_positions, white_fields
+        all_positions.add(position)
+        if is_white:
+            white_fields.add(position)
         elif current_color:
-            positions.remove(position)
+            white_fields.remove(position)
         if rotation:
             direction *= 0+1j # rotate left
         else:
@@ -40,7 +40,8 @@ def paint_the_panel(robot_instructions, first_white=False):
 
 robot_instructions = list(map(int, get_input('inputs/input11',',')))
 
-_, party_1 = paint_the_panel(robot_instructions)
+painted_at_least_once, _ = paint_the_panel(robot_instructions)
+party_1 = len(painted_at_least_once)
 print_solutions(party_1)
 
 panel_pt2, _ = paint_the_panel(robot_instructions, True)
