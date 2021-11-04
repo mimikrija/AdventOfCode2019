6a9e16c37814d8d46b7187c433541a72106bbfcb
Maja Gacesa <maja.gacesa@gmail.com>
2021-11-04 21:38:47 +0100

    [day10] party one with complex num angles/phases

--- python/10.py
+++ python/10.py
@@ -1,6 +1,14 @@
 # Day 10: Monitoring Station
 
 from santas_little_helpers import *
+from cmath import phase
 
-asteroids = {(x, y) for x, row in enumerate(get_input('inputs/input10'))
-                              for y, col in enumerate(row) if col=='#'}
+asteroids = {complex(x, -y) for x, row in enumerate(get_input('inputs/input10'))
+                           for y, col in enumerate(row) if col=='#'}
+
+def detected(station, asteroids):
+    angles = {phase(station-asteroid) for asteroid in asteroids if asteroid != station}
+    return angles
+
+party_1 = max(len(detected(station, asteroids)) for station in asteroids)
+print_solutions(party_1)
