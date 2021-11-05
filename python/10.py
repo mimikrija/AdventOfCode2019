4bf772d69dc8d20ef530c9d5e3343449fa748862
Maja Gacesa <maja.gacesa@gmail.com>
2021-11-05 22:14:23 +0100

    [day10] a lot of assumptions here ...
    since part 1 solution is greater than 200 it is not like we need to run
    in circles, we just need to sort the angles and find the closest
    asteroid on that line.
    
    to do that we:
    - adjusted `detected` to return a dict of angle: list of relative posit
    ions
    - calculated angle from the vertical axis (hence atan x/y) otherwise we
    would need to sort
    - sorted the relative positions per angle by magnitude to find the
    closest one to the station
    - pick up the 200th member of the sorted dict by angles

--- python/10.py
+++ python/10.py
@@ -1,18 +1,31 @@
 # Day 10: Monitoring Station
 
 from santas_little_helpers import *
-from cmath import phase
+from cmath import phase, pi, sqrt
+from math import atan2
+from collections import defaultdict
 
-asteroids = {complex(x, y) for y, row in enumerate(get_input('inputs/input10'))
+asteroids = {complex(x,y) for y, row in enumerate(get_input('inputs/input10'))
                             for x, col in enumerate(row) if col=='#'}
 
+def angle(num):
+    return atan2(num.real, num.imag)
+
 def detected(station, asteroids):
-    angles = {phase(station-asteroid) for asteroid in asteroids if asteroid != station}
+    angles = defaultdict(list)
+    for asteroid in asteroids:
+        if asteroid != station:
+            angles[angle(asteroid-station)].append(asteroid-station)
     return angles
-
-
 
 best_station, best_angles = max(((station, detected(station, asteroids)) for station in asteroids),
                                 key=lambda x: len(x[1]))
 party_1 = len(best_angles)
-print_solutions(party_1)
+
+magnitude = lambda x: sqrt(x.real**2 + x.imag**2).real
+
+angle_200 = sorted(best_angles.keys(),reverse=True)[199]
+asteroid_200 = min(best_angles[angle_200], key=magnitude) + best_station
+
+party_2 = int(asteroid_200.real*100 + asteroid_200.imag)
+print_solutions(party_1, party_2)
