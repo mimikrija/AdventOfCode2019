4c88b0ca1de85938cc8c8a2b790d04cd9e13fb53
Maja Gacesa <maja.gacesa@gmail.com>
2021-11-05 20:30:22 +0100

    [day10] fix mixup with coordinates

--- python/10.py
+++ python/10.py
@@ -3,15 +3,16 @@
 from santas_little_helpers import *
 from cmath import phase
 
-asteroids = {complex(x, -y) for x, row in enumerate(get_input('inputs/input10'))
-                            for y, col in enumerate(row) if col=='#'}
+asteroids = {complex(x, y) for y, row in enumerate(get_input('inputs/input10'))
+                            for x, col in enumerate(row) if col=='#'}
 
 def detected(station, asteroids):
     angles = {phase(station-asteroid) for asteroid in asteroids if asteroid != station}
     return angles
 
 
-best_station, best_angles = max([(station, detected(station, asteroids)) for station in asteroids],
+
+best_station, best_angles = max(((station, detected(station, asteroids)) for station in asteroids),
                                 key=lambda x: len(x[1]))
 party_1 = len(best_angles)
 print_solutions(party_1)
